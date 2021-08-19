6e10b396ddbc10aef60d39443f1e690fef1c8065
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-19 22:16:31 +0300

    add comments

--- py_accronyms.py
+++ py_accronyms.py
@@ -1,11 +1,11 @@
 user_text = str(input('Enter the phrase: '))
-
-text = user_text.split()
+print(user_text)
+text = user_text.split() #split the setence into separate strings
 print(text)
 
-a = " "
+a = " " #defining the variable that would store our first letter of the word
 
-for i in text:
-    a = a + str(i[0]).upper()
+for i in text: #loop through the splitted text
+    a = a + str(i[0]).upper()#store each first letter of the splitted text and convert it to upper case
 print(a)    
 
