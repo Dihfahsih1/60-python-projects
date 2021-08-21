dd6d17c18864bd128da879e749b918fee34efe09
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-22 00:13:48 +0300

    comment in code with what has been learnt

--- alarm_clock.py
+++ alarm_clock.py
@@ -2,10 +2,10 @@ from datetime import datetime
 from playsound import playsound
 
 alarm_time = input('Enter the time of the alarm to set: HH:MM:SS\n')
-alarm_hour=alarm_time[0:2]
-alarm_minute=alarm_time[3:5]
+alarm_hour=alarm_time[0:2]#store hours part of the input from index 0 to index 2(not inclusive)
+alarm_minute=alarm_time[3:5]#range or sequence from index 3 to 4 only 
 alarm_seconds=alarm_time[6:8]
-alarm_period =alarm_time[9:11].upper()
+alarm_period =alarm_time[9:11].upper() 
 print("Setting up alarm..")
 
 while True:
