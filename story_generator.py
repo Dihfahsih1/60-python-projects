210d9f9084d3b97660ee964c11148dba6d5dbf7b
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-23 23:13:42 +0300

    use requests in a py file

--- story_generator.py
+++ story_generator.py
@@ -1,4 +1,5 @@
 import random
+import requests
 when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On 20th Jan']
 who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
 name = ['Ali', 'Miriam','daniel', 'Hoouk', 'Starwalker']
@@ -6,3 +7,6 @@ residence = ['Barcelona','India', 'Germany', 'Venice', 'England']
 went = ['cinema', 'university','seminar', 'school', 'laundry']
 happened = ['made a lot of friends','Eats a burger', 'found a secret key', 'solved a mistery', 'wrote a book']
 print(random.choice(when) + ', ' + random.choice(who) + ' that lived in ' + random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
+
+x = requests.get('https://www.github.com')
+print(x.text)
