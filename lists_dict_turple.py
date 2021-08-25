2e4474a18f4d2e9de4cf33016932392101e0db7d
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-25 22:52:40 +0300

    learn lists and the operations perfomed on lists

---
+++ lists_dict_turple.py
@@ -0,0 +1,11 @@
+#list
+person = ['first_name', 'age','gendar']
+
+front_end=['html','css', 'bootstrap', 'javascript']
+
+back_end = ['python','nodejs','java','php']
+
+#back_end.extend(front_end)
+
+back_end += front_end
+print(back_end)
