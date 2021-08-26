b54293c8dccea4e0a63c35a168278e64a1451bb9
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-26 23:59:14 +0300

    operations on list data type

--- lists_dict_turple.py
+++ lists_dict_turple.py
@@ -5,7 +5,16 @@ front_end=['html','css', 'bootstrap', 'javascript']
 
 back_end = ['python','nodejs','java','php']
 
+#replace an item in the list
+back_end[1]='django'
+print(back_end)
 #back_end.extend(front_end)
 
-back_end += front_end
-print(back_end)
+# use += operate back_end += front_end
+#print(back_end)
+
+name = 'eduraka'
+name.upper()
+# name[3]='t' #strings are immutable
+
+  
