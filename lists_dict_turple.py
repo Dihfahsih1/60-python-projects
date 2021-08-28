fba175a13daaa0f2788f421723a279bf4e24f413
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-28 17:51:41 +0300

    deep learning of lists operations

--- lists_dict_turple.py
+++ lists_dict_turple.py
@@ -5,10 +5,38 @@ front_end=['html','css', 'bootstrap', 'javascript']
 
 back_end = ['python','nodejs','java','php']
 
+#deleting an item from a list
+del front_end[1]
+#print(front_end)
+
+#concatenating lists
+a,b=['aapple','mango'], [1, 2,3,4]
+
+a=[10, 11, 12]
+
+c = a+b
+print(c)
+
+#list compression
+
+#using for loop
+for i in c:
+    if i % 2 == 0:
+        #print(i) 
+        pass   
+
+#using list compression
+even_numbers=[ i for i in c if i % 2 == 0]
+print(even_numbers)
+
+#using lambda expression
+even_nos = list(filter(lambda x: (x % 2 == 0), c))
+print (even_nos)
+
 #replace an item in the list
 back_end[1]='django'
 
-#append a value to the list
+#append a value to the list this adds the new item to the end
 back_end.append('Appended value')
 
 #insert a value in the list at a given index
@@ -16,21 +44,21 @@ back_end.insert(2, 'Kotlin')
 
 
 #back_end.extend(front_end
-print(back_end)
+#print(back_end)
 
 #del back_end[2:]
 
 #slicing the list
 back=back_end[2:5]
-print(back)
+#print(back)
 
 #dimensional list
 
 a_list = [[[10, 20], 300], [400, 500], 600]
-print(a_list[0][0][0]) #print 20
+#print(a_list[0][0][0]) #print 20
 
 test = "mugoya", "dihfahsih" "akab"
-print(list(test))
+#print(list(test))
 
 # use += operate back_end += front_end
 #print(back_end)
@@ -52,7 +80,7 @@ lang['js'] = "ReactJs"
 
 #add a value to a dictionery
 lang['ry'] = 'Ruby and Rails'
-print(lang)
+#print(lang)
 #print(lang.get('js'))
 
 
@@ -61,11 +89,11 @@ print(lang)
 
 a_turple = ('ug','uganda', 'tz','tanzania')
 #a_turple[1] = 'adnagu'
-print(a_turple[1])
+#print(a_turple[1])
 
 #Set
-# => they can not be index, they have no duplicates
+# => they can not be indexed, they have no duplicates
 a_set = {12, 40, 'james', 12, 'jon', 'james'}
-print(a_set)
+#print(a_set)
 #print(a_set[2]) sets can not be assigned
   
