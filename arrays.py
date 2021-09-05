a9ea6613ca59814522519285f3cc5e5be4afe9bd
Dihfahsih1 <dihfahsihm@gmail.com>
2021-09-05 22:54:51 +0300

    learn turples

---
+++ arrays.py
@@ -0,0 +1,8 @@
+import array as arr
+
+sample_array = arr.array('H',[2, 3, 4] )
+
+print(sample_array)
+
+for i in sample_array:
+    print(i)
---
+++ learned/turples.py
@@ -0,0 +1,19 @@
+percent = (30,89,90)
+
+#turples are not mutable
+
+#they can also be created without using parenthesis
+b = 12, "turple", 50
+
+#you can assign sequences to a turple
+c = (12, 32, 10, 3, "list")
+#1, 2, 3, 4, 5 = c
+
+a = 1  
+d = 1,
+
+#a and d are different => type: int and type: turple respectively.
+
+print (type(a))
+
+print (type(d))
