6c7d044df9b6205623ca8337b547a5454b8ae6eb
Dihfahsih1 <dihfahsihm@gmail.com>
2021-09-17 21:38:43 +0300

    great  stuffs i didnt know this

--- learned/list_comprehension.py
+++ learned/list_comprehension.py
@@ -10,3 +10,10 @@ print(mylist)
 
 #conditioned list comprehension
 print([i for i in range(20) if i%2==0 if i%3==0])
+
+print(['Even' if i%2==0 else 'Old' for i in range(8)])
+
+#nested for loop for list comprehension
+for i in range(7,9):
+    for j in range(1,11):
+        print(f"{i}*{j}={i*j}")
