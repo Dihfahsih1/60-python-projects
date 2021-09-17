6cffc9c566f1d62cc3c5e288b70877cee2444862
Dihfahsih1 <dihfahsihm@gmail.com>
2021-09-17 22:12:19 +0300

    learn map function as well

--- learned/list_comprehension.py
+++ learned/list_comprehension.py
@@ -8,12 +8,19 @@ for i in "anxiety":
     mylist.append(i)
 print(mylist)
 
+
+
 #conditioned list comprehension
 print([i for i in range(20) if i%2==0 if i%3==0])
 
 print(['Even' if i%2==0 else 'Old' for i in range(8)])
 
 #nested for loop for list comprehension
-for i in range(7,9):
-    for j in range(1,11):
-        print(f"{i}*{j}={i*j}")
+def func1(n):
+    return n*n
+            
+            
+
+numbers=(2,3,5,6)
+y=map(func1, numbers)
+print(list(y))
