887c38b79628d61ae6e9bb9e5a3d2193ee7c9f99
Dihfahsih1 <dihfahsihm@gmail.com>
2021-09-23 06:24:27 +0300

    learn forloop and all the performances that it can have

---
+++ learned/loops.py
@@ -0,0 +1,31 @@
+#while loop
+a=3
+while a>0:
+    print(a)
+    a-=1
+    if a==1:break
+else:
+    print("reached 0")
+  
+#if we have a single statement    
+a=5 
+while a<0: print(a); a-=1
+
+#using range(start,endpoint, interval)
+#the range must be from less to great else the list would be empty
+#the interval in the reange can also be negative(from great to less)
+print("List of integers-" + str(list(range(12,2,-5))))
+
+#iterating a list using the for loop, this can also be done on the string
+list1=[1,4,6,7,10,11]
+for i in list1:
+    print(i)
+
+#iterating on the indices
+
+list2=["Holy Spirit", "God", "Jesus"]
+#for loop can also have else statement after all the code inside the for loop has been executed
+for i in range(len(list2)):
+    print(str(i) + " " + str(list2[i]))
+else:
+    print("all items have been printed")
