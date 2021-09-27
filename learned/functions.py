e749e502a498043ee7aca79ba38f45676a333ddf
Dihfahsih1 <dihfahsihm@gmail.com>
2021-09-27 05:27:54 +0300

    learn arguements in a function

--- learned/functions.py
+++ learned/functions.py
@@ -9,3 +9,15 @@ def recusive(n):
     return n*recusive(n-1)
 
 recusive(5)
+
+#arguments
+#passing keyword arguments
+# def greeting(name="User", age=22):
+#     print(f"Hello, {name}. You are {age} years old")
+# greeting(input("Enter your name:\n"), input("Enter Your age:\n"))
+
+#Arbitrary arguements, you literally don't know how many arguments you will be passing
+def people(*names):
+    for i in names:
+        print(f"{i}, hello")
+people('John', 'Mathew', 'Luke')
