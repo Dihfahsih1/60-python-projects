2186786b528acc3167003686ef825b6a660401b4
Dihfahsih1 <dihfahsihm@gmail.com>
2021-10-12 15:50:02 +0300

    wasted 2 hours

--- projects/file_demo.py
+++ projects/file_demo.py
@@ -1,5 +1,20 @@
-with open("srs.doc", 'rb') as rf:
-  with open("srs_copy.txt", 'wb') as wf:
-    for line in rf:
-      wf.write(line)
+import csv
+with open("list_of_emails.csv", 'r') as data_file:
+  csv_data = csv.reader(data_file)
+  str1 =""
+  for line in csv_data:
+    print(line)
+    username = str(line).split("@", 1)[0]
+    print(username)
+    if username.split(".", 1) == False:
+      first_name = username.split('.', 1)[0].capitalize()
+      print(first_name)
+      last_name = username.split('.', 1)[1].capitalize()
+      
+    # contains = name.split(".")[0]
+    # print(contains)
+    # if contains:
+    #   first_name = contains[0][0]
+    #   print(first_name)
+      
  
