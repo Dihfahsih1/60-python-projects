072dc088679a729403be4e20fd58ffae2770f53c
Dihfahsih1 <dihfahsihm@gmail.com>
2021-10-12 17:18:45 +0300

    convert list to string so as to use split on it

--- projects/file_demo.py
+++ projects/file_demo.py
@@ -1,20 +1,23 @@
 import csv
-with open("list_of_emails.csv", 'r') as data_file:
-  csv_data = csv.reader(data_file)
-  str1 =""
-  for line in csv_data:
-    print(line)
-    username = str(line).split("@", 1)[0]
-    print(username)
-    if username.split(".", 1) == False:
-      first_name = username.split('.', 1)[0].capitalize()
-      print(first_name)
-      last_name = username.split('.', 1)[1].capitalize()
-      
-    # contains = name.split(".")[0]
-    # print(contains)
-    # if contains:
-    #   first_name = contains[0][0]
-    #   print(first_name)
-      
- 
+
+email = ["dihfahsih.mugoya@gmail.com",'difas.mugoya@gmail.com']
+
+for lines in email:
+  line=str(lines) #convert list to string
+  username = line.split("@", 1)[0]
+  print(username)
+  try:
+    first_name = username.split('.', 1)[0].capitalize()
+    print(first_name)
+  except Exception as e:
+    print(e)
+    
+  try:
+    last_name = username.split('.', 1)[1].capitalize()
+    print(last_name)
+  except Exception as e:
+    print(e)
+        
+    
+        
+  
