5cea33278500756c514362733620d8ed461f2095
Dihfahsih1 <dihfahsihm@gmail.com>
2021-10-13 23:55:46 +0300

    append list names though still not as intended

--- projects/file_demo.py
+++ projects/file_demo.py
@@ -3,6 +3,7 @@ import csv
 #email = ["dihfahsih.mugoya@gmail.com",'difasmugoya@gmail.com']
 with open('list_of_emails.csv', 'r') as f:
   email = csv.reader(f)
+  new_list=[]
   for lines in email:
     line=str(lines) #convert list to string
     #username = line.split("@", 1)[0]
@@ -11,15 +12,19 @@ with open('list_of_emails.csv', 'r') as f:
     try:
       if len(name)>=2:
         first_name = name[0]
-        print("First Name: " +first_name)
+        new_list.append(first_name)
+        #print("First Name: " +first_name)
         
         last_name = name[1]
-        print("Last Name :" +last_name)
+        #print("Last Name :" +last_name)
+        new_list.append(last_name)
         
-      elif name != "":
+      else:
         print(f"Your username is  {username}")
     except Exception as e:
       print('error')
+  print(new_list)
+      
       
 
     
