ba8d2672f9fc332cb16ba6d3e8bbb8c3c31d9100
Dihfahsih1 <dihfahsihm@gmail.com>
2021-10-13 23:47:45 +0300

    use the csv file

--- projects/file_demo.py
+++ projects/file_demo.py
@@ -1,25 +1,26 @@
 import csv
 
-email = ["dihfahsih.mugoya@gmail.com",'difasmugoya@gmail.com']
-
-for lines in email:
-  line=str(lines) #convert list to string
-  #username = line.split("@", 1)[0]
-  username, _, _ = line.partition("@")
-  name = username.split(".")
-  try:
-    if len(name)>=2:
-      first_name = name[0]
-      print("First Name: " +first_name)
+#email = ["dihfahsih.mugoya@gmail.com",'difasmugoya@gmail.com']
+with open('list_of_emails.csv', 'r') as f:
+  email = csv.reader(f)
+  for lines in email:
+    line=str(lines) #convert list to string
+    #username = line.split("@", 1)[0]
+    username, _, _ = line.partition("@")
+    name = username.split(".")
+    try:
+      if len(name)>=2:
+        first_name = name[0]
+        print("First Name: " +first_name)
+        
+        last_name = name[1]
+        print("Last Name :" +last_name)
+        
+      elif name != "":
+        print(f"Your username is  {username}")
+    except Exception as e:
+      print('error')
       
-      last_name = name[1]
-      print("Last Name :" +last_name)
-      
-    else:
-      print(f"Your user name: {username}")
-  except Exception as e:
-    print('error')
-    
 
     
         
