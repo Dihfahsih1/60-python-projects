dd7c734b868c8a114a5955d23b95a1417977dd80
Dihfahsih1 <dihfahsihm@gmail.com>
2021-10-15 17:09:09 +0300

    don't print username in the terminal

--- projects/file_demo.py
+++ projects/file_demo.py
@@ -21,15 +21,16 @@ with open('list_of_emails.csv', 'r') as f:
         new_list.append(last_name)
         
       else:
-        print(f"Your username is  {username}")
+        pass
+        #print(f"Your username is  {username}")
     except Exception as e:
       print('error')
       
 
-#writing to a new file to store the fetched data      
-with open('new_list_of_emails.csv', 'w') as f:
-  new_email_list = str(new_list)
-  f.write(new_email_list)  
+  #writing to a new file to store the fetched data      
+  with open('new_list_of_emails.csv', 'w') as f:
+    new_email_list = str(new_list)
+    f.write(new_email_list)  
       
       
 
