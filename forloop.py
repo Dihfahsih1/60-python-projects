7ff82e8ff7f277d5eaea370f8013af9c1f454d04
Dihfahsih1 <dihfahsihm@gmail.com>
2021-12-30 00:17:16 +0300

    applying nested nested or inner for loops

--- forloop.py
+++ forloop.py
@@ -6,6 +6,12 @@ for price in prices:
   
 print(f"Total Price: {total}")
 
-for x in range(5):
-  for y in range(5):
-    print(f"({x}, {y})")
+numbers = [5, 2, 5, 2, 2 ]
+# for x in numbers:
+#   print('x' * x)
+
+for x in numbers:
+  output = ""
+  for y in range(x):
+    output += "x"
+  print(output)
