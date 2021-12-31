db14e31762667e6f3d1d1e65f6a1c496ec9e2975
Dihfahsih1 <dihfahsihm@gmail.com>
2021-12-31 23:16:23 +0300

    remove duplicates

--- lists.py
+++ lists.py
@@ -16,6 +16,14 @@ numbers.sort()
 numbers.reverse()
 numbers.pop()
 numbers.remove(40)
-
-
 print(numbers)
+
+
+#remove duplicates
+repeated_number= [10,1,10,20,30,40,50,10,30]
+uniques=[]
+for num in repeated_number:
+  if num not in uniques:
+    uniques.append(num)
+print(uniques)    
+    
