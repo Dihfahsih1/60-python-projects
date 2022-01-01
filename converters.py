7a9fe869a4812e5d94b8bf7fcaa00653f697ef68
Dihfahsih1 <dihfahsihm@gmail.com>
2022-01-01 20:44:56 +0300

    that was pretty simple

--- converters.py
+++ converters.py
@@ -1,27 +1,25 @@
-prices = [10,45,60]
+# prices = [10,45,60]
 
-total = 0
-for price in prices:
-  total += price
+# total = 0
+# for price in prices:
+#   total += price
   
-print(f"Total Price: {total}")
+# print(f"Total Price: {total}")
 
-numbers = [1,3,6,8,9,10,5,2 ]
+# numbers = [1,3,6,8,9,10,5,2 ]
+# # for x in numbers:
+# #   print('x' * x)
+
 # for x in numbers:
-#   print('x' * x)
-
-for x in numbers:
-  output = ""
-  for y in range(x):
-    output += "x"
-  print(output)
+#   output = ""
+#   for y in range(x):
+#     output += "x"
+#   print(output)
   
 #printing the largest number  
-class find_max(number):
+def find_max(number):
   max = number[0]
   for num in number:
     if num > max:
       max = num
   print(max)
-find_max()
-  
