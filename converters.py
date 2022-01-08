eff1c549e180b6c15c226a450a315442260a9c1d
Dihfahsih1 <dihfahsihm@gmail.com>
2022-01-08 20:56:31 +0300

    clean up code

--- converters.py
+++ converters.py
@@ -1,21 +1,4 @@
-# prices = [10,45,60]
 
-# total = 0
-# for price in prices:
-#   total += price
-  
-# print(f"Total Price: {total}")
-
-# numbers = [1,3,6,8,9,10,5,2 ]
-# # for x in numbers:
-# #   print('x' * x)
-
-# for x in numbers:
-#   output = ""
-#   for y in range(x):
-#     output += "x"
-#   print(output)
-  
 #printing the largest number  
 def find_max(number):
   max = number[0]
