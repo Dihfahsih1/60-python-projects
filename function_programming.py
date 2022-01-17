ddda56210fe5ea9c8f44742411f3920eadca6472
Dihfahsih1 <dihfahsihm@gmail.com>
2022-01-18 01:12:02 +0300

    use lambda function

--- function_programming.py
+++ function_programming.py
@@ -1,2 +1,11 @@
-def func():
-  print("am a function")
+  
+animals = ['dog','zebra','elephant', 'lion',]
+def reverse_len(s):
+  return -len(s)
+y=sorted(animals, key=reverse_len)
+print(y)
+
+# lambda function = <para_list>:<expression>
+
+result=(lambda a,b,c: (a+b+c)/4)(10,40,5)
+print("lamda function: " + str(result))
