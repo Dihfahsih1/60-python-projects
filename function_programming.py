fc4e1bca276604464f9df689a99b677bbdcbca3e
Dihfahsih1 <dihfahsihm@gmail.com>
2022-01-18 03:18:10 +0300

    using lambda function

--- function_programming.py
+++ function_programming.py
@@ -3,9 +3,13 @@ animals = ['dog','zebra','elephant', 'lion',]
 def reverse_len(s):
   return -len(s)
 y=sorted(animals, key=reverse_len)
-print(y)
-
+ 
 # lambda function = <para_list>:<expression>
 
 result=(lambda a,b,c: (a+b+c)/4)(10,40,5)
 print("lamda function: " + str(result))
+
+std=sorted(animals, key=lambda s: -len(s))
+using_sort = animals.sort()
+
+print(using_sort)
