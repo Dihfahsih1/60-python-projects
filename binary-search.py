c2b0acb8bd7bd0932398a5ab74491762b0b6efcc
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-21 21:57:43 +0300

    need to fix the error

--- binary-search.py
+++ binary-search.py
@@ -1,1 +1,15 @@
-def linear_search(numbers_list, number_to_find)
+def linear_search(numbers_list, number_to_find):
+  for index, element in enumerate(numbers_list):
+    if element == number_to_find:
+      return index 
+  return -1
+
+def binary_search(numbers_list,number_to_find):
+  pass
+
+if __name__ == '__main__':
+  numbers_list = [12,13,14,17,25,45,78]
+  number_to_find = input('Please enter the number to find: ')
+  
+  index = linear_search(numbers_list,number_to_find)
+  print(f'number found at index {index} using linear search')
