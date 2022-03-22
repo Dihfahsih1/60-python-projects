e18bce0727d8eff7bedce1ac4e43e7975c2d9595
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-22 04:23:11 +0300

    fixed: convert input into an int

--- binary-search.py
+++ binary-search.py
@@ -9,7 +9,8 @@ def binary_search(numbers_list,number_to_find):
 
 if __name__ == '__main__':
   numbers_list = [12,13,14,17,25,45,78]
-  number_to_find = input('Please enter the number to find: ')
+  number_to_find = int(input('Please enter the number to find: '))
+  
   
   index = linear_search(numbers_list,number_to_find)
   print(f'number found at index {index} using linear search')
