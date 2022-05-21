eb605c4fdb1b6058e26d9936343490303845904e
Dihfahsih1 <dihfahsihm@gmail.com>
2022-05-21 23:28:58 +0300

    successful inheritence

--- OOP/oop.py
+++ OOP/oop.py
@@ -1,4 +1,5 @@
 class Employee:
+  raise_amt = 0
   def __init__(self, first, last, pay):
     self.fname = first 
     self.lname = last 
@@ -12,9 +13,12 @@ class Employee:
     self.pay = int(self.pay * self.raise_amt)
     
 class Developer(Employee):
-  pass
+  raise_amt = 10.5
 
 dev_1 = Developer("Django", "Python", 700000)
+ 
+print(dev_1.pay)
+dev_1.apply_raise()
+print(dev_1.pay)
 
-fullname(self)
     
