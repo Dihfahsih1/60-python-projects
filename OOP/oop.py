9cffb6dedbd2e57df84f9fe61a6f16d1b91f62d9
Dihfahsih1 <dihfahsihm@gmail.com>
2022-05-22 00:26:05 +0300

    work on inheritance

--- OOP/oop.py
+++ OOP/oop.py
@@ -1,5 +1,5 @@
 class Employee:
-  raise_amt = 0
+  raise_amt = 100
   def __init__(self, first, last, pay):
     self.fname = first 
     self.lname = last 
@@ -7,18 +7,49 @@ class Employee:
     self.pay = pay
     
   def fullname(self):
-    return '{} {}.format(self.fname, self.lname)'
+    return '{} {}'.format(self.fname, self.lname)
   
   def apply_raise(self):
     self.pay = int(self.pay * self.raise_amt)
     
 class Developer(Employee):
-  raise_amt = 10.5
+  raise_amt = 1.10
+  def __init__(self, first, last, pay, pro_lang):
+    super().__init__(first, last, pay)
+    self.pro_lang = pro_lang
 
-dev_1 = Developer("Django", "Python", 700000)
- 
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
+class Manager(Employee):
+  def __init__(self, first, last, pay, employees=None):
+    super().__init__(first, last, pay)
+    if employees is None:
+      self.employees = []
+    else:
+      self.employees = employees
+    
+  def add_emp(self, emp):
+    if emp not in self.employees:
+      self.employees.append(emp)
+    
+  def remove_emp(self, emp):
+    if emp in self.employees:
+      self.employees.remove(emp) 
+      
+  def print_emp(self):
+    for emp in self.employees:
+      print('-->', emp.fullname())
+      
+        
+
+dev_1 = Developer("John", "Smith", 700000, 'Python')
+dev_2 = Developer("Yokana", "Nartin", 49999, 'Java') 
+
+
+mgr = Manager("Patrick", "Aine", 300000, [dev_2])
+print(mgr.email)
+mgr.remove_emp(dev_1)
+mgr.print_emp()
+#print(dev_1.pro_lang)
+# dev_1.apply_raise()
+# print(dev_1.pay)
 
     
